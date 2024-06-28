ebfa3d92ece387a8362e4eb4b533d7316e469602
sharifkhanmail21 <96617678+sharifkhanmail21@users.noreply.github.com>
2024-06-28 18:50:39 +0800

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,8 +2,9 @@ Variable "aws-region" {
   description = "Configure AWS Region"
   type = string
   default = "ap-sourth-a"
-
+}
 Variable "user-name" {
   description = "IAM User"
   type = list(string)
   default = ["demo1","demo2","demo3"]
+}
