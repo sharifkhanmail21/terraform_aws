e89f76a65ca7ce5051b22e91bbc12c366cb249f5
sharifkhanmail21 <sharifkhanmail21@gmail.com>
2024-06-30 16:35:57 +0800

    test

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,12 @@
-# #VPC Configuration
-# resource "aws_vpc" "main" {
-#   cidr_block       = "10.0.0.0/16"
-#   instance_tenancy = "default"
+#VPC Configuration
+resource "aws_vpc" "main-vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
 
-#   tags = {
-#     Name = "main"
-#   }
-# }
+  tags = {
+    Name = "main"
+  }
+}
 
 # #Subnet Configuration
 # resource "aws_subnet" "main" {
