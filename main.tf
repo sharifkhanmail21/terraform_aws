1dfe35d90045728bd51ea965567629e12c6ae1ba
sharifkhanmail21 <96617678+sharifkhanmail21@users.noreply.github.com>
2024-06-30 16:47:14 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 # provider "aws" {
 #   region     = "ap-south-1"
-#   access_key = "AKIATCKARAQL2QDQBTJ4"
-#   secret_key = "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"
+#   access_key = "Access Key"
+#   secret_key = "Secret Key"
 #   #Key should be store in file and give reference
 # }
 
 provider "aws" {
-  shared_config_files      = "C://Users/shari/.aws/conf"
+  shared_config_files      = "C://Users/shari/.aws/config"
   shared_credentials_files = "C://Users/shari/.aws/credentials"
   profile                  = "techlab"
 }
